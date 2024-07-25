811caee483ed4e07bf89adb4cfe2914d5d04fbc8
Pavlo Smokorovskiy <pavvelsm@gmail.com>
2024-07-24 23:45:19 -0400

    Added Testee.java

---
+++ src/main/java/com/pavvels/assessment/model/Testee.java
@@ -0,0 +1,25 @@
+package com.pavvels.assessment.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document
+@NoArgsConstructor
+@Getter
+@Setter
+public class Testee {
+    @Id
+    private String id;
+    private String testeeId;
+    private List<SubjectScore> subjects;
+
+    public Testee(String testeeId, List<SubjectScore> subjects) {
+        this.testeeId = testeeId;
+        this.subjects = subjects;
+    }
+
+}
