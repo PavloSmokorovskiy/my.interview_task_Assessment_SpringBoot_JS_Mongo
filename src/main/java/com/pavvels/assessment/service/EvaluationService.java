5da4d4ca86bf4493f4cc58058333eae0aea0e5a8
Pavlo Smokorovskiy <pavvelsm@gmail.com>
2024-07-26 00:44:12 -0400

    Extended getEvaluatedScores logic

--- src/main/java/com/pavvels/assessment/service/EvaluationService.java
+++ src/main/java/com/pavvels/assessment/service/EvaluationService.java
@@ -3,18 +3,21 @@ package com.pavvels.assessment.service;
 import com.pavvels.assessment.model.SubjectScore;
 import com.pavvels.assessment.model.Testee;
 import com.pavvels.assessment.repository.TesteeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.*;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EvaluationService {
 
     private final TesteeRepository testeeRepository;
-
-    public EvaluationService(TesteeRepository testeeRepository) {
-        this.testeeRepository = testeeRepository;
-    }
+    private final MongoTemplate mongoTemplate;
 
     public void saveTestScores(Testee testee) {
         for (SubjectScore subject : testee.getSubjects()) {
@@ -27,7 +30,28 @@ public class EvaluationService {
 
     @SuppressWarnings("unused")
     public List<Testee> getEvaluatedScores(String testeeIds, String subjects, String totalRange, String averageRange, String scoreRange) {
-        return testeeRepository.findAll();
+        Query query = new Query();
+
+        if (testeeIds != null && !testeeIds.isEmpty()) {
+            List<String> ids = Arrays.asList(testeeIds.split(","));
+            query.addCriteria(Criteria.where("testeeId").in(ids));
+        }
+
+        if (subjects != null && !subjects.isEmpty()) {
+            List<String> subjectList = Arrays.asList(subjects.split(","));
+            query.addCriteria(Criteria.where("subjects.subject").in(subjectList));
+        }
+
+        if (totalRange != null && !totalRange.isEmpty()) {
+            String[] range = totalRange.split("-");
+            double min = Double.parseDouble(range[0]);
+            double max = Double.parseDouble(range[1]);
+            query.addCriteria(Criteria.where("totalScore").gte(min).lte(max));
+        }
+
+        query.with(Sort.by(Sort.Direction.DESC, "totalScore")).with(Sort.by(Sort.Direction.ASC, "testeeId"));
+
+        return mongoTemplate.find(query, Testee.class);
     }
 
     private void calculateTotalAndAverage(Testee testee) {
